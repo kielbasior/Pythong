034422b31e1ccd83617ac4c04bf9819bf7c4dc98
Jakub <bciejor@gmail.com>
2021-07-05 02:02:37 +0200

    3 zad zrobione, jutro dokoncze reszte

---
+++ 01/zestaw 1.py
@@ -0,0 +1,49 @@
+
+"""
+#zad 1
+for x in range(100, 1000):
+  liczby = [int(x) for x in str(x)]
+  if liczby[2] > liczby[1] and liczby[1] > liczby[0]:
+    print(x)
+
+#zad 2
+polish_digits = ["ą", "ć", "ę", "ł", "ń", "ó", "ś", "ź", "ż"]
+english_digits = ["a", "c", "e", "l", "n", "o", "s", "z", "z"]
+def zad1(string):
+    for x in range(len(string)):
+        for y in range(len(polish_digits)):
+            if string[x].lower() == polish_digits[y].lower():
+                string = string.replace(string[x], english_digits[y])
+    return string
+print(zad1("Szczęście jest tak bardzo blisko"))
+"""
+#zad 3
+slowa = ["korniszon", "maciek", "serdelki", "masło"]
+def pary(list):
+    for x in range(len(list)):
+        new_item = (list[x], len(list[x]))
+        list.remove(list[x])
+        list.insert(x, new_item)
+    return list
+print(pary(slowa))
+
+#zad 4
+dates = [{'d': 4, 'm': 10, 'y': 2000},
+        {'d': 2, 'm': 11, 'y': 2001},
+        {'d': 10, 'm': 9, 'y': 2000},
+        {'d': 4, 'm': 5, 'y': 2000},
+        {'d': 5, 'm': 3, 'y': 2001},
+        {'d': 6, 'm': 1, 'y': 2000},
+        {'d': 2, 'm': 5, 'y': 2000},
+        {'d': 1, 'm': 2, 'y': 2001},
+        {'d': 10, 'm': 9, 'y': 2000}]
+
+#skip bo mnie wkurwia samo patrzenie na to
+
+#zad 5
+lista = [[1, 2, 3], ["Abba", "aff"], [-2, 4, 5]]
+
+
+
+
+
